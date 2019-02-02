ec2c85edfbfc4c0c90dd1fc8572cb2e534d514b3
Peter A. Higgins <pahiggins@cenabo.com>
2019-02-02 09:39:04 +0000

    Add engine-demo file

--- index.js
+++ index.js
@@ -1,36 +1,70 @@
+// require('dotenv').config();
+
 const { ApolloServer } = require('apollo-server');
+const isEmail = require('isemail');
+
 const typeDefs = require('./src/schema');
 const resolvers = require('./src/resolvers');
-const isEmail = require('isemail');
 const { createStore } = require('./src/utils');
 
 const LaunchAPI = require('./src/datasources/launch');
 const UserAPI = require('./src/datasources/user');
 
+const internalEngineDemo = require('./src/engine-demo');
+
+// creates a sequelize connection once. NOT for every request
 const store = createStore();
 
+// set up any dataSources our resolvers need
+const dataSources = () => ({
+  launchAPI: new LaunchAPI(),
+  userAPI: new UserAPI({ store }),
+});
+
+// the function that sets up the global context for each resolver, using the req
+const context = async ({ req }) => {
+  // simple auth check on every request
+  const auth = (req.headers && req.headers.authorization) || '';
+  const email = new Buffer(auth, 'base64').toString('ascii');
+
+  // if the email isn't formatted validly, return null for user
+  if (!isEmail.validate(email)) return { user: null };
+  // find a user by their email
+  const users = await store.users.findOrCreate({ where: { email } });
+  const user = users && users[0] ? users[0] : null;
+
+  return { user: { ...user.dataValues } };
+};
+
+// Set up Apollo Server
 const server = new ApolloServer({
-  context: async ({ req }) => {
-    // simple auth check on every request
-    const auth = (req.headers && req.headers.authorization) || '';
-    const email = Buffer.from(auth, 'base64').toString('ascii');
-
-    // if the email isn't formatted validly, return null for user
-    if (!isEmail.validate(email)) return { user: null };
-    // find a user by their email
-    const users = await store.users.findOrCreate({ where: { email } });
-    const user = users && users[0] ? users[0] : null;
-
-    return { user: { ...user.dataValues } };
-  },
   typeDefs,
   resolvers,
-  dataSources: () => ({
-    launchAPI: new LaunchAPI(),
-    userAPI: new UserAPI({ store }),
-  })
+  dataSources,
+  context,
+  engine: {
+    apiKey: process.env.ENGINE_API_KEY,
+    ...internalEngineDemo,
+  },
 });
 
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+// Start our server if we're not in a test env.
+// if we're in a test env, we'll manually start it in a test
+// if (process.env.NODE_ENV !== 'test')
+
+  server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
+
+// export all the important pieces for integration/e2e tests to use
+module.exports = {
+  dataSources,
+  context,
+  typeDefs,
+  resolvers,
+  ApolloServer,
+  LaunchAPI,
+  UserAPI,
+  store,
+  server,
+};
